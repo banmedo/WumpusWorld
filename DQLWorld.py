8cd299b9d11cee3c70a450c7a9d29595664b73f1
Nishanta Khanal <khanal.nishant@gmail.com>
2021-06-27 22:18:25 -0400

    add dql agent and world

---
+++ DQLWorld.py
@@ -0,0 +1,28 @@
+from src.agent.dql import DQLAgent
+
+from src.world.environment import Environment
+
+from utils import log
+
+ACTION_DEPTH = 20
+
+def run_episode(env, agent, percept, action_depth=0):
+    if (action_depth >= ACTION_DEPTH):
+        return 0
+
+    next_action = agent.next_action(percept)
+    log(f"Action : {next_action}")
+    next_percept = env.act(next_action)
+    log(env)
+    log(next_percept)
+    return next_percept.reward + (run_episode(env, agent, next_percept, action_depth+1) if not next_percept.is_terminated else 0)
+
+initial_env = Environment(4, 4, 0, False)
+initial_percept = initial_env.get_percepts(skip_reward=True)
+log("Start a new world!")
+log(initial_env)
+log(initial_percept)
+agent = DQLAgent()
+
+total_reward = run_episode(initial_env, agent, initial_percept)
+log(f"Total reward: {total_reward}")
---
+++ src/agent/dql.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.insert(0, "../..")
+
+from src.world.agent import Agent
+from src.world.utils import rand_index
+
+class DQLAgent(Agent):
+    ACTIONS = ["forward","turn_left","turn_right","grab","shoot","climb"]
+
+    def next_action(self, _):
+        return self.ACTIONS[rand_index(len(self.ACTIONS))]
